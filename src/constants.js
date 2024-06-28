9da5572dd26eefe275626c52eda8e1077ca15f74
Ishaan-03 <saxenaishaan03@gmail.com>
2024-06-28 09:18:41 +0530

    fix all register controller bug

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const DB_NAME="PlayPulse";
+export const DB_NAME="PlayPulse"
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -25,7 +25,7 @@ const registerUser= asyncHandler( async(req,res)=>{
     throw new ApiError(400,"all feilds are required")    
    }
 
-    const existedUser =User.findOne(
+    const existedUser = await User.findOne(
        { $or: [
             {username},
             {email}
@@ -37,14 +37,22 @@ if (existedUser) {
 }
 
 const avatarLocalPath =req.files?.avatar[0]?.path;
-const coverImageLocalpPath = req.files?.coverImage[0]?.path;
+// const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+
+let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+
+
 
 if (!avatarLocalPath) {
     throw new ApiError(400,"avatar is required");
 }
 
 const avatar = await uploadOnCloudinary(avatarLocalPath);
-const coverImage = await uploadOnCloudinary(coverImageLocalpPath); 
+const coverImage = await uploadOnCloudinary(coverImageLocalPath); 
 
 
 if (!avatar) {
